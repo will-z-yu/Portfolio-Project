b03c9eb588965e0c907cde4402d786df9daf7993
Will <zhenjiay@uci.edu>
2019-06-25 16:17:54 -0700

    finished slick function

--- src/slick_func.js
+++ src/slick_func.js
@@ -2,6 +2,12 @@ $(document).ready(function(){
     $('.slick-slideshow').slick({
         infinite: true,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        //auto play
+        autoplay: true,
+        autoplaySpeed: 2000,
+        //fade
+        fade: true,
+        speed: 1000
     });
 });
